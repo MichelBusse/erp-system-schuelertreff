2be436b768eeaefeb3529447bf160914f6a5cef7
Lyn Fugmann <lyn.fugmann@m-to-b.com>
2022-03-13 21:20:33 +0100

    Add WebSocket/Gateway example

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,31 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import { io } from 'socket.io-client'
+import { nanoid } from 'nanoid'
 import logo from './logo.svg'
 import './App.scss'
 
-const App = () => {
+type Message = {
+  data: string
+  id: string
+}
+
+const socket = io(import.meta.env.VITE_API_URL, { transports: ['websocket'] })
+
+const App: React.FC = () => {
   const [count, setCount] = useState(0)
+  const [messages, setMessages] = useState<Message[]>([])
+  const [msgInput, setMsgInput] = useState('')
+
+  const handleSubmitMessage = () => {
+    socket.emit('message', { data: msgInput, id: nanoid() })
+    setMsgInput('')
+  }
+
+  useEffect(() => {
+    socket.on("connect_error", err => console.error(`connect_error due to ${err.message}`))
+
+    socket.on('message', (msg: Message) => setMessages(m => [...m, msg]))
+  }, [])
 
   return (
     <div className="App">
@@ -16,6 +38,18 @@ const App = () => {
           </button>
         </p>
         <p>API url: {import.meta.env.VITE_API_URL}</p>
+
+        <ul>
+          {messages.map(msg => <li key={msg.id}>{msg.data}</li>)}
+        </ul>
+
+        <input
+          type="text"
+          value={msgInput}
+          onChange={e => setMsgInput(e.target.value)}
+          onKeyPress={e => (e.key == 'Enter') ? handleSubmitMessage() : ''}
+        />
+
         <p>
           Edit <code>App.tsx</code> and save to test HMR updates.
         </p>
