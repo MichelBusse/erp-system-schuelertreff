a0ce2cbbd3d7434b0f2de304cb0cff06e47fcb35
Conne <conrad.guenther@m-to-b.com>
2022-04-12 14:10:03 +0200

    little Changes
    Structered the Code a little bit, added comments and little more style

--- client/src/components/Calendar.tsx
+++ client/src/components/Calendar.tsx
@@ -6,33 +6,42 @@ import TableCell from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
-import { Dayjs } from 'dayjs'
+import dayjs, { Dayjs } from 'dayjs'
 
+//Properties of the Component
 type Props = {
   date: Dayjs,
   teachers: Array
 }
 
+//Properties of the Columns
 interface Column {
-  id: 'Teacher' | 'Montag' | 'Dienstag' | 'Mittwoch' | 'Donnerstag' | 'Freitag';
+  id: 'Teacher' | 'Montag' | 'Dienstag' | 'Mittwoch' | 'Donnerstag' | 'Freitag'; //list of possible IDs
   number: number;
-  label: string;
-  minWidth?: number;
-  align?: 'right' | 'center';
+  label: string; //ColumnName
+  minWidth?: number; 
+  backgroundColor: string;
+  align?: 'right' | 'center'; //Position (default is left)
+  type: 'day' | 'noday';
 }
 
-var minWidth = 130
-var color = "#E1FF95"
-var columnNumber = 0
+var minWidth = 130 //default minWidth of Column
+var color1 = "#E9FDB7"  //green bright
+var color2 = "#E1FF95" //green dark
+var rowNumber = 0 //counter for rows to change color
+var backgroundColor = "" //backgorundColor for rows
 
+//Array of all Columns with there Propertys
 const columns: readonly Column[] = [
-  { id: 'Montag', number: 1, label: 'Montag', minWidth: minWidth , align: 'center'},
-  { id: 'Dienstag', number: 2, label: 'Dienstag', minWidth: minWidth , align: 'center'},
-  { id: 'Mittwoch', number: 3, label: 'Mittwoch', minWidth: minWidth , align: 'center'},
-  { id: 'Donnerstag', number: 4, label: 'Donnerstag', minWidth: minWidth , align: 'center'},
-  { id: 'Freitag', number: 5, label: 'Freitag', minWidth: minWidth , align: 'center'},
+  { id: 'Montag', number: -1, label: '', minWidth: minWidth , backgroundColor: color1, align: 'center', type: 'noday'},
+  { id: 'Montag', number: 1, label: 'Montag', minWidth: minWidth , backgroundColor: color2, align: 'center', type: 'day'},
+  { id: 'Dienstag', number: 2, label: 'Dienstag', minWidth: minWidth , backgroundColor: color1, align: 'center', type: 'day'},
+  { id: 'Mittwoch', number: 3, label: 'Mittwoch', minWidth: minWidth , backgroundColor: color2, align: 'center', type: 'day'},
+  { id: 'Donnerstag', number: 4, label: 'Donnerstag', minWidth: minWidth , backgroundColor: color1, align: 'center', type: 'day'},
+  { id: 'Freitag', number: 5, label: 'Freitag', minWidth: minWidth , backgroundColor: color2, align: 'center', type: 'day'},
 ];
 
+//Type of the Datas:
 // interface Data {
 //   Montat: string;
 //   Dienstag: string;
@@ -41,31 +50,46 @@ const columns: readonly Column[] = [
 //   Freitag: string;
 // }
 
+//Calendar Component:
 const Calendar: React.FC<Props> = ({ date, teachers }) => {
 
   return (
     <Paper sx={{ width: '100%', overflow: 'hidden' }}>
       <TableContainer sx={{ maxHeight: 440 }}>
         <Table stickyHeader aria-label="sticky table">
+          {/* Tablehead (.map iterates through all columns and print them into single cells): */}
           <TableHead>
             <TableRow>
-            <TableCell style={{ minWidth: 170 , backgroundColor: color}}></TableCell>
               {columns.map((column) => {
-                color = (columnNumber%2 == 0)? "#E9FDB7" : "#E1FF95"
-                columnNumber++
-                return(
-                <TableCell key={column.id} align={column.align} style={{ minWidth: column.minWidth, backgroundColor: color}}>
-                  {date.day(column.number).format('dddd - DD.MM')}
-                </TableCell>
-              )})}
+                // if its a column of day (all except the first on):
+                if(column.type == "day"){
+                  return(
+                    <TableCell key={column.id} align={column.align} style={{ minWidth: column.minWidth, backgroundColor: column.backgroundColor}}>
+                      {/* Content of each cell (Day + Date): */}
+                      <p style={{margin: 0, height: 13}}>{column.label}</p>
+                      <p style={{margin: 0, fontSize: 10}}>{date.day(column.number).format('DD.MM.YYYY')}</p>
+                    </TableCell>
+                  )
+                // if its not a column of day (the first one)
+                } else{
+                  return(
+                    <TableCell key={column.id} style={{ minWidth: column.minWidth, backgroundColor: column.backgroundColor}}>
+                      {/* Content of each cell (empty): */}
+                    </TableCell>
+                  )
+                }
+              })}
             </TableRow>
           </TableHead>
+          {/* Tablebody (.map iterates through all teachers and prints them in rows): */}
           <TableBody>
             {teachers
               .map((teacher) => {
+                backgroundColor = (rowNumber%2==0)? color1 : color2
+                rowNumber++
                 return (
                   <TableRow hover role="checkbox" key={teacher.id}>
-                    <TableCell>
+                    <TableCell style={{ backgroundColor: backgroundColor}}>
                       {teacher.name}
                     </TableCell>
                     <TableCell style={{ borderStyle: "solid", borderColor: "grey", borderWidth: 1}}>1</TableCell>
