46fe6048ec6bf7117960a29dabff0914d20fcb40
Lyn Fugmann <lyn.fugmann@m-to-b.com>
2022-05-06 20:51:39 +0200

    Refactor Calendar styles

--- client/src/components/Calendar.tsx
+++ client/src/components/Calendar.tsx
@@ -1,104 +1,69 @@
-import * as React from 'react';
-import Paper from '@mui/material/Paper';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import dayjs, { Dayjs } from 'dayjs';
-import './calendar.scss';
-import { styled } from '@mui/system';
-import CalendarControl from './CalendarControl';
-import { grey } from '@mui/material/colors';
+import {
+  Paper,
+  Table,
+  TableBody,
+  TableContainer,
+  TableHead,
+  TableRow,
+  TableCell,
+} from '@mui/material'
+import dayjs, { Dayjs } from 'dayjs'
+import CalendarControl from './CalendarControl'
+import styles from './Calendar.module.scss'
 
-//Type of the Array Teachers
-type teachers = {
+type teacher = {
   id: number
   name: string
 }
-//Properties of the Component
+
 type Props = {
   date: Dayjs
-  teachers: Array<teachers>
+  teachers: Array<teacher>
   setOpen: Function
   setDate: Function
 }
-//Styling:
-const StyledPaper = styled(Paper, {})({
-  width: '100%',
-  overflow: 'hidden',
-  borderRadius: 25,
-});
 
-const StyledCellContent = styled(TableCell, {})({
-  borderRightStyle: 'solid',
-  borderWidth: 0.5,
-  borderColor: '#CDCDCD',
-  backgroundColor: 'white'
-});
+const Calendar: React.FC<Props> = ({ date, teachers, setOpen, setDate }) => (
+  <Paper className={styles.wrapper}>
+    <CalendarControl date={date} setDate={setDate} />
 
-const StyledCellWeekday = styled(TableCell, {})({
-  width: "17%",
-  minWidth: 150
-});
+    <TableContainer sx={{ maxHeight: 'calc(100vh - 104px)' }}>
+      <Table stickyHeader>
 
-const StyledCellTeacherName = styled(TableCell, {})({
-  width: '15em',
-  height: '5em',
-  borderStyle: 'none',
-  borderRightStyle: 'solid',
-  borderWidth: 0.5,
-  borderColor: '#CDCDCD',
-  paddingTop: 5,
-  paddingBottom: 5,
-});
+        <TableHead>
+          <TableRow>
+            <TableCell />
 
-//Calendar Component:
-const Calendar: React.FC<Props> = ({ date, teachers, setOpen, setDate }) => {
-  return (<>
-    <StyledPaper>
-      <CalendarControl date={date} setDate={setDate} />
-      <TableContainer sx={{ maxHeight: 'calc(100vh - 104px)'}}>
-        <Table stickyHeader aria-label="sticky table">
-          {/* Tablehead (.map iterates through all columns and print them into single cells): */}
-          <TableHead>
-            <TableRow>
-              <StyledCellTeacherName sx={{borderStyle: "none"}} className="coloredCell"></StyledCellTeacherName>
-              {[1,2,3,4,5].map((d) => {
-                // if its a column of day (all except the first on):
-                  return(
-                    <StyledCellWeekday align="center" key={d} className="coloredCell">
-                      <div style={{fontSize: '1.2rem'}}>{date.day(d).format("dddd")}</div>
-                      <div style={{fontSize: '0.8rem', margin: '-5px'}}>{date.day(d).format('DD.MM.YYYY')}</div>
-                    </StyledCellWeekday>
-                  )
-              })}
+            {[1, 2, 3, 4, 5].map((d) => (
+              /* if its a column of day (all except the first one):*/
+              <TableCell key={d} align="center">
+                <div style={{ fontSize: '1.2rem' }}>
+                  {date.day(d).format('dddd')}
+                </div>
+                <div style={{ fontSize: '0.8rem', margin: '-5px' }}>
+                  {date.day(d).format('DD.MM.YYYY')}
+                </div>
+              </TableCell>
+            ))}
+          </TableRow>
+        </TableHead>
+
+        <TableBody>
+          {teachers.map((teacher) => (
+            <TableRow key={teacher.id} hover role="checkbox">
+              <TableCell onClick={() => setOpen({ state: true, info: teacher.name })}>
+                {teacher.name}
+              </TableCell>
+
+              {[1, 2, 3, 4, 5].map((d) => (
+                <TableCell key={d}>{d}</TableCell>
+              ))}
             </TableRow>
-          </TableHead>
-          {/* Tablebody (.map iterates through all teachers and prints them in rows): */}
-          <TableBody>
-            {teachers
-              .map((teacher) => {
-                return (
-                  <TableRow hover role="checkbox" key={teacher.id} className="coloredCell2">
-                    <StyledCellTeacherName onClick={() => setOpen({state: true, info: teacher.name})} >
-                      {teacher.name}
-                    </StyledCellTeacherName>
-                    <StyledCellContent>1</StyledCellContent>
-                    <StyledCellContent>2</StyledCellContent>
-                    <StyledCellContent>3</StyledCellContent>
-                    <StyledCellContent>4</StyledCellContent>
-                    <StyledCellContent>5</StyledCellContent>
-                  </TableRow>
-                );
-              })}
-          </TableBody>
-        </Table>
-      </TableContainer>
-    </StyledPaper>
-   </>
-  );
-}
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  </Paper>
+)
 
 export default Calendar
