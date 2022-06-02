04f6c392d6c2c213d24fd584afa32367a3aabcb1
Lyn Fugmann <lyn.fugmann@m-to-b.com>
2022-06-03 01:55:30 +0200

    Timetable as DataGrid

--- client/src/components/Calendar.tsx
+++ client/src/components/Calendar.tsx
@@ -1,88 +1,96 @@
-import {
-  Paper,
-  Table,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableHead,
-  TableRow,
-} from '@mui/material'
+import AddIcon from '@mui/icons-material/Add'
+import { Fab, Paper } from '@mui/material'
+import { DataGrid, GridColDef, GridRowsProp } from '@mui/x-data-grid'
 import { Dayjs } from 'dayjs'
 
 import { SideMenu } from '../pages/timetable'
 import styles from './Calendar.module.scss'
 import CalendarControl from './CalendarControl'
 
-type teacher = {
-  id: number
-  name: string
-}
-
 type Props = {
   date: Dayjs
-  teachers: Array<teacher>
   setOpen: (open: SideMenu) => void
   setDate: (date: Dayjs) => void
+  openDialog: () => void
 }
 
-const lessons = [
-  { Day: 'Mon', Subject: ['Deu'] },
-  { Day: 'Tue', Subject: ['Deu', 'Ma'] },
-  { Day: 'Wed', Subject: ['Ph', 'Fr', 'Lat'] },
-  { Day: 'Thu', Subject: ['Deu', 'Eng', 'Bio', 'Geo'] },
-  { Day: 'Fri', Subject: ['Eth'] },
+const rows: GridRowsProp = [
+  { id: 1, teacher: 'Hello', mon: 'World' },
+  { id: 2, teacher: 'DataGridPro', mon: 'is Awesome' },
+  { id: 3, teacher: 'MUI', mon: 'is Amazing', wed: 'test' },
 ]
 
-const Calendar: React.FC<Props> = ({ date, teachers, setOpen, setDate }) => (
-  <Paper className={styles.wrapper}>
-    <CalendarControl date={date} setDate={setDate} />
+const Calendar: React.FC<Props> = ({ date, setOpen, setDate, openDialog }) => {
+  const columns: GridColDef[] = [
+    { field: 'teacher', headerName: '', width: 200, sortable: false },
+    {
+      field: 'mon',
+      headerName: 'Montag\n' + date.day(1).format('DD.MM.YYYY'),
+      width: 150,
+      sortable: false,
+    },
+    {
+      field: 'tue',
+      headerName: 'Dienstag\n' + date.day(2).format('DD.MM.YYYY'),
+      width: 150,
+      sortable: false,
+    },
+    {
+      field: 'wed',
+      headerName: 'Mittwoch\n' + date.day(3).format('DD.MM.YYYY'),
+      width: 150,
+      sortable: false,
+    },
+    {
+      field: 'thu',
+      headerName: 'Donnerstag\n' + date.day(4).format('DD.MM.YYYY'),
+      width: 150,
+      sortable: false,
+    },
+    {
+      field: 'fri',
+      headerName: 'Freitag\n' + date.day(5).format('DD.MM.YYYY'),
+      width: 150,
+      sortable: false,
+    },
+  ]
 
-    <TableContainer sx={{ maxHeight: 'calc(100vh - 104px)' }}>
-      <Table stickyHeader>
-        <TableHead>
-          <TableRow>
-            <TableCell />
+  return (
+    <Paper
+      className={styles.wrapper}
+      sx={{ width: columns.reduce((p, c) => p + (c.width ?? 100), 0) }}
+    >
+      <CalendarControl date={date} setDate={setDate} />
 
-            {[1, 2, 3, 4, 5].map((d) => (
-              /* if its a column of day (all except the first one):*/
-              <TableCell key={d} align="center">
-                <div style={{ fontSize: '1.2rem' }}>
-                  {date.day(d).format('dddd')}
-                </div>
-                <div style={{ fontSize: '0.8rem', margin: '-5px' }}>
-                  {date.day(d).format('DD.MM.YYYY')}
-                </div>
-              </TableCell>
-            ))}
-          </TableRow>
-        </TableHead>
+      <DataGrid
+        style={{
+          flexGrow: 1,
+          border: 'none',
+        }}
+        rows={rows}
+        columns={columns}
+        disableColumnMenu={true}
+        disableSelectionOnClick={true}
+        onCellClick={(params) => {
+          if (params.colDef.field === 'teacher')
+            setOpen({ state: true, info: `${params.id}` })
+        }}
+      />
 
-        <TableBody>
-          {teachers.map((teacher) => (
-            <TableRow key={teacher.id} hover role="checkbox">
-              <TableCell
-                onClick={() => setOpen({ state: true, info: teacher.name })}
-              >
-                {teacher.name}
-              </TableCell>
-
-              {lessons.map((day) => (
-                <TableCell key={day.Day} className={styles.lessonCell}>
-                  <div className={styles.lessonContainer}>
-                    {day.Subject.map((subject, i) => (
-                      <div key={i} className={styles.lesson}>
-                        {subject}
-                      </div>
-                    ))}
-                  </div>
-                </TableCell>
-              ))}
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
-  </Paper>
-)
+      <Fab
+        color="primary"
+        aria-label="add"
+        style={{
+          position: 'absolute',
+          bottom: 64,
+          right: 16,
+        }}
+        onClick={openDialog}
+      >
+        <AddIcon />
+      </Fab>
+    </Paper>
+  )
+}
 
 export default Calendar
--- client/src/pages/timetable.tsx
+++ client/src/pages/timetable.tsx
@@ -1,7 +1,6 @@
 import 'dayjs/locale/de'
 
-import AddIcon from '@mui/icons-material/Add'
-import { Box, Fab } from '@mui/material'
+import { Box } from '@mui/material'
 import dayjs from 'dayjs'
 import weekOfYear from 'dayjs/plugin/weekOfYear'
 import { useState } from 'react'
@@ -18,25 +17,6 @@ export type SideMenu = {
   info: string
 }
 
-const teachers = [
-  { id: 1, name: 'Prof. der Magie Fugi' },
-  { id: 2, name: 'Oberhaupt Michel' },
-  { id: 3, name: 'Dr. Shishos Cönos' },
-  { id: 4, name: 'Klimmzug Teubner' },
-  { id: 5, name: 'Eggi Extrem' },
-  { id: 6, name: 'Außendienstler Bley' },
-  { id: 7, name: 'Angela Merkel' },
-  { id: 8, name: 'Luis Günthersohn' },
-  { id: 9, name: 'Litsch' },
-  { id: 10, name: 'Paul Albert Hendinger' },
-  { id: 11, name: 'Eggi Extrem' },
-  { id: 12, name: 'Außendienstler Bley' },
-  { id: 13, name: 'Angela Merkel' },
-  { id: 14, name: 'Luis Günthersohn' },
-  { id: 15, name: 'Litsch' },
-  { id: 16, name: 'Paul Albert Hendinger' },
-]
-
 const Timetable: React.FC = () => {
   const [open, setOpen] = useState<SideMenu>({ state: false, info: 'Standard' })
   const [date, setDate] = useState(dayjs())
@@ -44,33 +24,29 @@ const Timetable: React.FC = () => {
   const [render, setRender] = useState(0)
 
   return (
-    <Box sx={{ p: 4 }}>
-      <Calendar
-        date={date}
-        teachers={teachers}
-        setOpen={setOpen}
-        setDate={setDate}
-      />
-
-      <HiddenMenu open={open} setOpen={setOpen} />
-
-      <Fab
-        color="primary"
-        aria-label="add"
-        style={{
-          position: 'absolute',
-          bottom: 42,
-          right: 42,
-        }}
-        onClick={() => {
-          // dialog component is re-created each time
-          // -> data will be fetched on button press
-          setRender(render + 1)
-          setDialogOpen(true)
+    <Box sx={{ p: 4, height: '100%' }}>
+      <Box
+        sx={{
+          height: '100%',
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
         }}
       >
-        <AddIcon />
-      </Fab>
+        <Calendar
+          date={date}
+          setOpen={setOpen}
+          setDate={setDate}
+          openDialog={() => {
+            // dialog component is re-created each time
+            // -> data will be fetched on button press
+            setRender(render + 1)
+            setDialogOpen(true)
+          }}
+        />
+      </Box>
+
+      <HiddenMenu open={open} setOpen={setOpen} />
 
       {!!render && (
         <ContractDialog
