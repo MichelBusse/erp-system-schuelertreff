93ba7fd9e5d5dfc127d18c2c3ccfbecbfeb72e08
Michel Busse <michel.busse@m-to-b.com>
2022-06-23 17:49:23 +0200

    Adjusted AddTeacherDialog

--- client/src/components/AddTimes.tsx
+++ client/src/components/AddTimes.tsx
@@ -5,12 +5,14 @@ import AddIcon from '@mui/icons-material/Add'
 import DeleteIcon from '@mui/icons-material/Delete'
 import {
   FormControl,
+  FormLabel,
   Grid,
   IconButton,
   InputLabel,
   MenuItem,
   Select,
   Stack,
+  Typography,
 } from '@mui/material'
 import Avatar from '@mui/material/Avatar'
 import List from '@mui/material/List'
@@ -70,93 +72,109 @@ const AddTimes: React.FC<Props> = ({ data, setData }) => {
     setData((data) => ({ ...data, timesAvailable: newTimes }))
   }
 
+  const elements = data.timesAvailable.map((time) => {
+    const timeText =
+      `${dayjs().day(time.dow).format('dddd')}: ` +
+      `${time.start?.format('HH:mm')} - ${time.end?.format('HH:mm')}`
+
+    return (
+      <ListItem
+        key={time.id}
+        secondaryAction={
+          <IconButton
+            edge="end"
+            aria-label="delete"
+            onClick={() => deleteTime(time.id)}
+          >
+            <DeleteIcon />
+          </IconButton>
+        }
+      >
+        <ListItemAvatar>
+          <Avatar>
+            <AccessTimeIcon />
+          </Avatar>
+        </ListItemAvatar>
+        <ListItemText primary={timeText} />
+      </ListItem>
+    )
+  })
+
   return (
     <>
-      <Stack direction="row" spacing={2} mt={2}>
-        <EqualStack direction="row" spacing={2}>
-          <FormControl>
-            <InputLabel id="DowLable">Wochentag</InputLabel>
-            <Select
-              id="dow"
-              label="Wochentag"
-              value={times.dow}
-              onChange={(event) =>
-                //TODO fix types
-                setTimes((data) => ({
-                  ...data,
-                  dow: event.target.value,
-                }))
-              }
-            >
-              <MenuItem value={1}>Montag</MenuItem>
-              <MenuItem value={2}>Dienstag</MenuItem>
-              <MenuItem value={3}>Mittwoch</MenuItem>
-              <MenuItem value={4}>Donnerstag</MenuItem>
-              <MenuItem value={5}>Freitag</MenuItem>
-            </Select>
-          </FormControl>
-          <BetterTimePicker
-            label="Startzeit"
-            minutesStep={5}
-            required={true}
-            maxTime={times.end?.subtract(30, 'm')}
-            value={times.start}
-            onChange={(value) => {
-              setTimes((data) => ({ ...data, start: value }))
-            }}
-            clearValue={() => {
-              setTimes((data) => ({ ...data, start: null }))
-            }}
-          />
-          <BetterTimePicker
-            label="Endzeit"
-            minutesStep={5}
-            required={true}
-            minTime={times.start?.add(30, 'm')}
-            value={times.end}
-            onChange={(value) => {
-              setTimes((data) => ({ ...data, end: value }))
-            }}
-            clearValue={() => {
-              setTimes((data) => ({ ...data, end: null }))
-            }}
-          />
-        </EqualStack>
-        <IconButton onClick={addTime} sx={{ alignSelf: 'center' }}>
-          <AddIcon />
-        </IconButton>
-      </Stack>
-      <Grid item>
-        <List dense={true}>
-          {data.timesAvailable.map((time) => {
-            const timeText =
-              `${dayjs().day(time.dow).format('dddd')}: ` +
-              `${time.start?.format('HH:mm')} - ${time.end?.format('HH:mm')}`
-
-            return (
-              <ListItem
-                key={time.id}
-                secondaryAction={
-                  <IconButton
-                    edge="end"
-                    aria-label="delete"
-                    onClick={() => deleteTime(time.id)}
-                  >
-                    <DeleteIcon />
-                  </IconButton>
+      <FormControl sx={{ marginTop: '10px' }}>
+        <FormLabel>Verfügbarkeit</FormLabel>
+        <Stack direction="row" spacing={2}>
+          <EqualStack direction="row" spacing={2}>
+            <FormControl>
+              <InputLabel id="DowLable">Wochentag</InputLabel>
+              <Select
+                id="dow"
+                label="Wochentag"
+                value={times.dow}
+                onChange={(event) =>
+                  //TODO fix types
+                  setTimes((data) => ({
+                    ...data,
+                    dow: event.target.value,
+                  }))
                 }
               >
-                <ListItemAvatar>
-                  <Avatar>
-                    <AccessTimeIcon />
-                  </Avatar>
-                </ListItemAvatar>
-                <ListItemText primary={timeText} />
-              </ListItem>
-            )
-          })}
-        </List>
-      </Grid>
+                <MenuItem value={1}>Montag</MenuItem>
+                <MenuItem value={2}>Dienstag</MenuItem>
+                <MenuItem value={3}>Mittwoch</MenuItem>
+                <MenuItem value={4}>Donnerstag</MenuItem>
+                <MenuItem value={5}>Freitag</MenuItem>
+              </Select>
+            </FormControl>
+            <BetterTimePicker
+              label="Startzeit"
+              minutesStep={5}
+              required={true}
+              maxTime={times.end?.subtract(30, 'm')}
+              value={times.start}
+              onChange={(value) => {
+                setTimes((data) => ({ ...data, start: value }))
+              }}
+              clearValue={() => {
+                setTimes((data) => ({ ...data, start: null }))
+              }}
+            />
+            <BetterTimePicker
+              label="Endzeit"
+              minutesStep={5}
+              required={true}
+              minTime={times.start?.add(30, 'm')}
+              value={times.end}
+              onChange={(value) => {
+                setTimes((data) => ({ ...data, end: value }))
+              }}
+              clearValue={() => {
+                setTimes((data) => ({ ...data, end: null }))
+              }}
+            />
+          </EqualStack>
+          <IconButton onClick={addTime} sx={{ alignSelf: 'center' }}>
+            <AddIcon />
+          </IconButton>
+        </Stack>
+        <Grid item>
+          <List
+            dense={true}
+            sx={{
+              backgroundColor: '#f5f5f5',
+              borderRadius: '4px',
+              margin: '5px 0',
+            }}
+          >
+            {elements.length > 0 ? (
+              elements
+            ) : (
+              <ListItem key={1}>Immer verfügbar</ListItem>
+            )}
+          </List>
+        </Grid>
+      </FormControl>
     </>
   )
 }
