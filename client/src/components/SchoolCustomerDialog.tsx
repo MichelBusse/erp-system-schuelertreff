38dd11b5f2d6b945968978e26d1b1f8a6860353a
ConradGünther <guenther.conrad.2001@gmail.com>
2022-06-25 11:54:25 +0200

    add SchoolType to Dialog

--- client/src/components/SchoolCustomerDialog.tsx
+++ client/src/components/SchoolCustomerDialog.tsx
@@ -1,9 +1,11 @@
 import {
+  Autocomplete,
   Button,
   Dialog,
   DialogActions,
   DialogContent,
   DialogTitle,
+  Stack,
   TextField,
 } from '@mui/material'
 import { useState } from 'react'
@@ -11,6 +13,7 @@ import { useState } from 'react'
 import { useAuth } from '../components/AuthProvider'
 import { schoolCustomer } from '../types/user'
 import { formValidation } from './FormValidation'
+import { SchoolType } from '../types/enums'
 
 type Props = {
   open: boolean
@@ -20,6 +23,7 @@ type Props = {
 
 const defaultFormData = {
   schoolName: '',
+  schoolType: '',
   city: '',
   postalCode: '',
   street: '',
@@ -59,78 +63,127 @@ const SchoolCustomerDialog: React.FC<Props> = ({
     <Dialog open={open}>
       <DialogTitle>Lehrkraft hinzufügen</DialogTitle>
       <DialogContent>
-        <TextField
-          helperText={errors.schoolName}
-          id="schoolName"
-          label="Schulname"
-          variant="outlined"
-          required={true}
-          sx={{ margin: '10px', marginLeft: '0px', width: '94%' }}
-          value={data.schoolName}
-          onChange={(event) =>
-            setData((data) => ({ ...data, schoolName: event.target.value }))
-          }
-        />
-        <TextField
-          helperText={errors.city}
-          id="city"
-          label="Stadt"
-          variant="outlined"
-          required={true}
-          sx={{ margin: '10px', marginLeft: '0px', width: '60%' }}
-          value={data.city}
-          onChange={(event) =>
-            setData((data) => ({ ...data, city: event.target.value }))
-          }
-        />
-        <TextField
-          helperText={errors.postalCode}
-          id="postalCode"
-          label="Postleitzahl"
-          variant="outlined"
-          required={true}
-          sx={{ margin: '10px', width: '30%' }}
-          value={data.postalCode}
-          onChange={(event) =>
-            setData((data) => ({ ...data, postalCode: event.target.value }))
-          }
-        />
-        <TextField
-          helperText={errors.street}
-          id="street"
-          label="Straße"
-          variant="outlined"
-          required={true}
-          sx={{ margin: '10px', marginLeft: '0px', width: '94%' }}
-          value={data.street}
-          onChange={(event) =>
-            setData((data) => ({ ...data, street: event.target.value }))
-          }
-        />
-        <TextField
-          helperText={errors.email}
-          id="email"
-          label="E-Mail Adresse"
-          variant="outlined"
-          required={true}
-          sx={{ margin: '10px', marginLeft: '0px', width: '60%' }}
-          value={data.email}
-          onChange={(event) =>
-            setData((data) => ({ ...data, email: event.target.value }))
-          }
-        />
-        <TextField
-          helperText={errors.phone}
-          id="phone"
-          label="Telefonnummer"
-          variant="outlined"
-          required={true}
-          sx={{ margin: '10px', width: '30%' }}
-          value={data.phone}
-          onChange={(event) =>
-            setData((data) => ({ ...data, phone: event.target.value }))
-          }
-        />
+        <Stack
+          direction={'column'}
+          rowGap={3}
+          alignItems={'stretch'}
+          sx={{ paddingTop: '15px' }}
+        >
+          <Stack direction={'row'} columnGap={2}>
+            <TextField
+              helperText={errors.schoolName}
+              id="schoolName"
+              label="Schulname"
+              variant="outlined"
+              required={true}
+              fullWidth
+              value={data.schoolName}
+              onChange={(event) =>
+                setData((data) => ({ ...data, schoolName: event.target.value }))
+              }
+            />
+          </Stack>
+          <Stack direction={'row'} columnGap={2}>
+            <Autocomplete
+              fullWidth
+              multiple
+              id="schoolTypes"
+              options={[
+                SchoolType.GRUNDSCHULE,
+                SchoolType.OBERSCHULE,
+                SchoolType.GYMSEK1, 
+                SchoolType.GYMSEK2,
+              ]}
+              getOptionLabel={(option) => {
+                switch (option) {
+                  case SchoolType.GRUNDSCHULE:
+                    return 'Grundschule'
+                  case SchoolType.OBERSCHULE:
+                    return 'Oberschule'
+                  case SchoolType.GYMSEK1:
+                    return 'Gymnasium Sek. 1'
+                  case SchoolType.GYMSEK2:
+                    return 'Gymnasium Sek. 2'
+                  default:
+                    return ''
+                }
+              }}
+              renderInput={(params) => (
+                <TextField {...params} variant="outlined" label="Schularten" />
+              )}
+              value={data.schoolType}
+              onChange={(_, value) =>
+                setData((data) => ({ ...data, schoolTypes: value }))
+              }
+            />
+          </Stack>
+          <Stack direction={'row'} columnGap={2}>
+            <TextField
+              helperText={errors.city}
+              id="city"
+              label="Stadt"
+              variant="outlined"
+              required={true}
+              sx={{ width: '70%' }}
+              value={data.city}
+              onChange={(event) =>
+                setData((data) => ({ ...data, city: event.target.value }))
+              }
+            />
+            <TextField
+              helperText={errors.postalCode}
+              id="postalCode"
+              label="Postleitzahl"
+              variant="outlined"
+              required={true}
+              sx={{ width: '30%' }}
+              value={data.postalCode}
+              onChange={(event) =>
+                setData((data) => ({ ...data, postalCode: event.target.value }))
+              }
+            />
+          </Stack>
+          <Stack direction={'row'} columnGap={2}>
+            <TextField
+              helperText={errors.street}
+              id="street"
+              label="Straße"
+              variant="outlined"
+              required={true}
+              fullWidth
+              value={data.street}
+              onChange={(event) =>
+                setData((data) => ({ ...data, street: event.target.value }))
+              }
+            />
+          </Stack>
+          <Stack direction={'row'} columnGap={2}>
+            <TextField
+              helperText={errors.email}
+              id="email"
+              label="E-Mail Adresse"
+              variant="outlined"
+              required={true}
+              sx={{ width: '60%' }}
+              value={data.email}
+              onChange={(event) =>
+                setData((data) => ({ ...data, email: event.target.value }))
+              }
+            />
+            <TextField
+              helperText={errors.phone}
+              id="phone"
+              label="Telefonnummer"
+              variant="outlined"
+              required={true}
+              sx={{ width: '40%' }}
+              value={data.phone}
+              onChange={(event) =>
+                setData((data) => ({ ...data, phone: event.target.value }))
+              }
+            />
+          </Stack>
+        </Stack>
       </DialogContent>
       <DialogActions>
         <Button onClick={closeForm}>Abbrechen</Button>
