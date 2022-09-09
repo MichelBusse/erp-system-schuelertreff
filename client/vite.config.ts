17a746a2f97379ded9c78f0462a96e2f117422af
Michel Busse <michel.busse@m-to-b.com>
2022-09-09 17:00:49 +0200

    Fixed FormValidation and edited preferences of manifest

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -13,6 +13,24 @@ export default defineConfig({
       devOptions: {
         enabled: true,
       },
+      manifest: {
+        name: 'Schülertreff',
+        short_name: 'Schülertreff',
+        description: 'Offizielle Schülertreff-Nachhilfe-App',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'logo-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'logo-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
     }),
   ],
 })
